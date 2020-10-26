c0a87d4fcd29230ccb864681bc110bc208f49074
eikrt <e.i.korte@gmail.com>
2020-10-26 15:40:00 +0200

    initial commit

---
+++ test/personal_web_app/core_test.clj
@@ -0,0 +1,7 @@
+(ns personal-web-app.core-test
+  (:require [clojure.test :refer :all]
+            [personal-web-app.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
